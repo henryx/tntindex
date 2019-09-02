1566c6fbea4e5eb57ce97c923580ff582369d43b
Enrico Bianchi <enrico.bianchi@antaresvision.com>
2019-09-02 13:48:46 +0200

    Used custom format for decoding data

--- index/data.go
+++ index/data.go
@@ -9,14 +9,29 @@ package index
 
 import "time"
 
+type Time struct {
+	time.Time
+}
+
+const format = "2006-01-02T15:04:05"
+
+func (t *Time) UnmarshalCSV(data []byte) error {
+	tt, err := time.Parse(format, string(data))
+	if err != nil {
+		return err
+	}
+	*t = Time{Time: tt}
+	return nil
+}
+
 type data struct {
-	Date     time.Time `csv:"DATA"`
-	Hash     string    `csv:"HASH"`
-	Topic    int       `csv:"TOPIC"`
-	Post     int       `csv:"POST"`
-	Author   string    `csv:"AUTORE"`
-	Title    string    `csv:"TITOLO"`
-	Desc     string    `csv:"DESCRIZIONE"`
-	Size     int       `csv:"DIMENSIONE"`
-	Category int       `csv:"CATEGORIA"`
+	Date     Time   `csv:"DATA"`
+	Hash     string `csv:"HASH"`
+	Topic    int    `csv:"TOPIC"`
+	Post     int    `csv:"POST"`
+	Author   string `csv:"AUTORE"`
+	Title    string `csv:"TITOLO"`
+	Desc     string `csv:"DESCRIZIONE"`
+	Size     int    `csv:"DIMENSIONE"`
+	Category int    `csv:"CATEGORIA"`
 }
