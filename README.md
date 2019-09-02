92d386850c44d0891122c53f6500272fcf4fe9c0
Enrico Bianchi <enrico.bianchi@antaresvision.com>
2019-09-02 10:14:38 +0200

    Started project

---
+++ README.md
@@ -0,0 +1,1 @@
+# TNTVillage index and Search tool
---
+++ main.go
@@ -0,0 +1,16 @@
+/*
+   Copyright (C) 2019 Enrico Bianchi (enrico.bianchi@gmail.com)
+   Project       tntindex
+   Description   An TNTVillage dump indexer and search tool
+   License       GPL version 2 (see LICENSE for details)
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+	fmt.Println("Hello world!")
+}
