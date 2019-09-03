044a838723f7baf3c791f9e06f7a3238d1d3d289
Enrico Bianchi <enrico.bianchi@antaresvision.com>
2019-09-03 18:27:52 +0200

    Started search function

---
+++ search/search.go
@@ -0,0 +1,14 @@
+/*
+   Copyright (C) 2019 Enrico Bianchi (enrico.bianchi@gmail.com)
+   Project       tntindex
+   Description   An TNTVillage dump indexer and search tool
+   License       GPL version 2 (see LICENSE for details)
+*/
+
+package search
+
+import "tntindex/database"
+
+func Search(db *database.DB, key *string) error {
+	return nil
+}
