83a06c9c5561fbc92ac3e9eea07da095998905f3
Enrico Bianchi <enrico.bianchi@antaresvision.com>
2019-09-03 18:53:13 +0200

    Started data extraction

--- search/search.go
+++ search/search.go
@@ -10,5 +10,11 @@ package search
 import "tntindex/database"
 
 func Search(db *database.DB, key *string) error {
+	// TODO: manage extracted data
+	_, err := db.SearchPost(*key)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
