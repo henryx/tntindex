a852acfc86211e72eebe1465f81258e9f28078c2
Enrico Bianchi <enrico.bianchi@gmail.com>
2021-12-28 15:35:43 +0100

    Added tracker add to magnet

--- main.go
+++ main.go
@@ -12,20 +12,27 @@ import (
 	"tntindex/database"
 	"tntindex/index"
 	"tntindex/search"
+	"tntindex/tracker"
 
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 func main() {
 	var db database.DB
+	var fileName *string
 
 	// Index command
 	cmdIndex := kingpin.Command("index", "Index a file")
-	fileName := cmdIndex.Arg("filename", "Filename to index").Required().String()
+	fileName = cmdIndex.Arg("filename", "Filename to index").Required().String()
 
 	// Search command
 	cmdSearch := kingpin.Command("search", "Search a torrent")
 	term := cmdSearch.Arg("term", "Term to search").Required().String()
+
+	// Tracker command
+	cmdTracker := kingpin.Command("tracker", "Add trackers to magnet link from file")
+	fileName = cmdTracker.Flag("file", "A file that contains a list of trackers").Short('f').Required().String()
+	magnet := cmdTracker.Arg("magnet", "A torrent magnet link").Required().String()
 
 	kingpin.HelpFlag.Short('h')
 
@@ -42,6 +49,10 @@ func main() {
 	case cmdSearch.FullCommand():
 		if err := search.Search(&db, term); err != nil {
 			log.Fatalln("Error when searching data:", err)
+		}
+	case cmdTracker.FullCommand():
+		if err := tracker.Generate(fileName, magnet); err != nil {
+			log.Fatalln("Error when opening tracker list file:", err)
 		}
 	}
 }
---
+++ tracker/tracker.go
@@ -0,0 +1,32 @@
+/*
+   Copyright (C) 2019 Enrico Bianchi (enrico.bianchi@gmail.com)
+   Project       tntindex
+   Description   An TNTVillage dump indexer and search tool
+   License       GPL version 2 (see LICENSE for details)
+*/
+
+package tracker
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func Generate(fileName, magnet *string) error {
+	f, err := os.Open(*fileName)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+
+	line := fmt.Sprintf("%s", magnet)
+	for scanner.Scan() {
+		line = fmt.Sprintf("%s&%s", line, scanner.Text())
+	}
+	fmt.Println("Magnet with trackers:")
+	fmt.Println(line)
+
+	return nil
+}
